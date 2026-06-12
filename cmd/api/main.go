ff2e5c5e74f7f4e559f7dd426414f1efa076a795
Dafy Daag <daf4daag@MacBook-Air-Dafy.local>
2026-06-12 18:17:44 +0500

    Added by Graceful Shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"restapi-test/internal/handlers"
+	"syscall"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/health", handlers.HealthCheck)
 
-	fmt.Println("Serve start in http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Ошибка сервера: %s\n", err)
+		}
+	}()
+	log.Println("Serve start in http://localhost:8080")
+
+	<-quit
+	log.Println("Получен сигнал завершения. Инициализация Graceful Shutdown...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Ошибка при остановке сервера: %s\n", err)
+	}
+
+	log.Println("Сервер успещно остановлен")
 }
